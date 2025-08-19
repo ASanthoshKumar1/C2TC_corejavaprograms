61c9ba41d24cbbbac6b8559d8cee2261d032bf09
santh <santh@Santhosh>
2025-08-19 14:40:24 +0530

    Single inheritNCE COMPPLETED

---
+++ java_sample/src/Singleinheritance/SlinheritanceDemo.java
@@ -0,0 +1,14 @@
+package Singleinheritance;
+
+public class SlinheritanceDemo {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Trainer t=new Trainer(112,"Ashwini","puducherry",11,"B.Tech ICB","chennai");
+		System.out.println(t);
+		Person p=new Person(11,"ashi","puducherry");
+		System.out.println(p);
+
+	}
+
+
+}
