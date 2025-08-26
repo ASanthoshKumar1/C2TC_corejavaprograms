5c44fb557639e16175c8e55722753540ad294dae
santh <santh@Santhosh>
2025-08-26 09:30:11 +0530

    DAy11 Completed

---
+++ java_sample/src/TNSIF/DAYeleven/v1/Demo.java
@@ -0,0 +1,13 @@
+package TNSIF.DAYeleven.v1;
+
+public class Demo {
+	
+
+	public static void main(String[] args) {
+		System.out.println("the program continues....");
+		int data=100/0;
+	}
+	
+
+
+}
